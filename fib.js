fe1b0a38b0d38a71f43c9c341ea945169365d9ac
William J Reid <willjreid@gmail.com>
2017-08-11 15:36:41 -0700

    wrapping up notes and begining quiz

--- fib.js
+++ fib.js
@@ -51,3 +51,16 @@ for (var i = 0; i < allQuestions.length; i++) {
   var thisQuestion = allQuestions[i];
   thisQuestion();
 }
+
+/* Problem 1 (this is your demo that we'll solve in class)
+Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the parameters and values that were input/calculated in the function:
+"The sum of 4 and 7 is 11."
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.
+For TODO item, be sure to change it to say DONE when you have successfully completed the task*/
+
+// TODO: Write your code here
+function sum(a,b){
+  var result = a + b;
+  var output = 'The sum of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, output];
+}
